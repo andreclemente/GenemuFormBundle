1d9c9ecac04b9904e49df25e74ae9bc8fa808c08
André Clemente <andreclemente@users.noreply.github.com>
2022-09-13 12:17:49 +0100

    Update FormExtension.php

--- Twig/Extension/FormExtension.php
+++ Twig/Extension/FormExtension.php
@@ -14,6 +14,7 @@ namespace Genemu\Bundle\FormBundle\Twig\Extension;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormRendererInterface;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 
 /**
  * FormExtension extends Twig with form capabilities.
@@ -46,8 +47,8 @@ class FormExtension extends AbstractExtension
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('form_javascript', array($this, 'renderJavascript'), array('is_safe' => array('html'))),
-            new \Twig_SimpleFunction('form_stylesheet', null, array(
+            new TwigFunction('form_javascript', array($this, 'renderJavascript'), array('is_safe' => array('html'))),
+            new TwigFunction('form_stylesheet', null, array(
                 'is_safe' => array('html'),
                 'node_class' => 'Symfony\Bridge\Twig\Node\SearchAndRenderBlockNode',
             )),
